56886b8db18b715ce3148a2155a5ea5aee76a23c
tatsuya828828 <kkjy011894@gmail.com>
2020-07-15 10:25:39 +0900

    [finish]07-05

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,19 @@
-import java.util.Iterator;
-import java.util.Properties;
-
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("ご利用のJavaバージョン:");
-		// JVMは内部にMap<String, String>を持っていて、動作に関する様々な情報を格納、利用している
-		// このJVMが持つ文字列ペアの集合をシステムプロパティという
-		// システムプロパティには、JVM生成時に様々な値が自動的に設定される
-		// システムプロパティから、あるペアの情報を取得したい場合にはgetPropertyメソッドにキーを指定する
-		// またgetPropertiesメソッドを使うことによって得られるPropertiesインスタンスのメソッドを使えば、
-		// ペアの値を取り出したりセットしたりできる
-		// java.versionキーは実行中のJREのバージョンを値として紐づけている
-		System.out.println(System.getProperty("java.version"));
-		Properties p = System.getProperties();
-		Iterator<String> i = p.stringPropertyNames().iterator();
-		System.out.println("[システムプロパティ一覧]");
-		while(i.hasNext()) {
-			String key = i.next();
-			System.out.println(key + " = ");
-			System.out.println(System.getProperty(key));
-		}
-
-		// Javaは基本的にはどのようなOSでも同様に動作するが100%ではない
-		// 例えば下のプログラムは動かすOSによっては、エスケープシーケンスが改行と意味しない場合がある
-		System.out.println("本日は\n晴天なり");
-		// line.separatorシステムプロパティを利用すると現在動作中のOSが採用している改行コードが自動的に格納されることになっている
-		// よって、どのOSでも正しく改行される
-		final String BR = System.getProperty("line.separator");
-		System.out.println("本日は"+ BR +"晴天なり");
-
-		// setPropertyを使えば文字列であればシステムプロパティに設定することもできる
-		System.setProperty("rpg.version", "0.3");
-		System.setProperty("rpg.author", "ミナト");
-		String ver = System.getProperty("rpg.version");
-		String author = System.getProperty("rpg.author");
-		System.out.println("RPG: スッキリ魔王征伐 ver"+ ver);
-		System.out.println("Developed by "+ author);
-
-		// システムプロパティは、どのようなクラスからも直接読み書きできる便利な格納領域だが
-		// 不具合の原因にもなりやすいため濫用は禁物
+		// getRuntimeメソッドを呼び出しRuntimeインスタンスを取得することでJVMの現在のメモリ状態を取得するメソッドが使える
+		// freeMemoryメソッドは残りのメモリ容量を返す
+		// /1024/1024でMB単位で表示ができる
+		long f = Runtime.getRuntime().freeMemory()/1024/1024;
+		System.out.println("残りのメモリ:"+ f +"MB");
+		// totalMemoryメソッドは、現在の総容量を表す
+		long f2 = Runtime.getRuntime().totalMemory()/1024/1024;
+		System.out.println("メモリの総容量:"+ f2 +"MB");
+		// maxMemoryメソッドは、メモリ総容量の限界値を表す
+		// 限界値とは、JVMのメモリが不足しそうな場合に、OSから追加のメモリ割り当てを受けた場合の総容量
+		// OSからの追加メモリ割り当ては、必要に応じて自動的に行われる
+		// しかし際限なく許すとOSや他のアプリケーションがメモリ不足に陥ってしまうため、
+		// JVMには追加割り当ての限界が設定されることが一般的である
+		long f3 = Runtime.getRuntime().maxMemory()/1024/1024;
+		System.out.println("メモリの限界値:"+ f3 +"MB");
 	}
 }
